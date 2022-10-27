26024f52d53fa95125124e7f40be28755b42138d
Cory Chu <cory@gwlab.page>
2022-10-27 09:08:55 -0500

    add auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import { ChakraProvider, Flex } from "@chakra-ui/react";
+
+
+import NavBar from '../components/NavBar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <ChakraProvider>
+        <NavBar/>
+        <Flex pt="60px" />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UserProvider>
+  );
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,39 @@
 import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
+import { useUser } from '@auth0/nextjs-auth0';
+
+function Profile() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    user && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <p>{user.sub}</p>
+      </div>
+    )
+  );
+}
+
+
+
+
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        <a href="/api/auth/login">Login</a>
+        <br/>
+        <a href="/api/auth/logout">Logout</a>
+      </h1>
+      <Profile/>
+    </>
   )
 }
