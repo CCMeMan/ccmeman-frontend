06317acc82f49fdf982e865d9d2ba986d544d39f
Cory Chu <cory@gwlab.page>
2022-11-03 18:56:22 -0500

    change to Tailwind on Group page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -19,240 +19,241 @@ import {
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import Image from "next/image";
-import { FaCheck, FaExclamation, FaRegCalendarAlt, FaInfoCircle } from "react-icons/fa";
+import {
+  FaCheck,
+  FaExclamation,
+  FaRegCalendarAlt,
+  FaInfoCircle,
+} from "react-icons/fa";
 import { useRouter } from "next/router";
-
 
 import { useUser } from "@auth0/nextjs-auth0";
 import BeatLoader from "react-spinners/BeatLoader";
 
-
-
-
 function SignInButton() {
-    const { user, error, isLoading } = useUser();
-    const router = useRouter();
-    const textColor = useColorModeValue("gray.600", "gray.300");
-    const loginBtnBg = useColorModeValue("yellow.300", "yellow.600");
-    
-    // Loading
-    if (isLoading) {
-      return (
-        <Button
-          colorScheme="blue"
-          variant="ghost"
-          size="md"
-          ml="10px"
-          mr="10px"
-          spinner={<BeatLoader size={8} color="white" />}
-          isLoading
-        />
-      );
-    }
+  const { user, error, isLoading } = useUser();
+  const router = useRouter();
+  const textColor = useColorModeValue("gray.600", "gray.300");
+  const loginBtnBg = useColorModeValue("yellow.300", "yellow.600");
 
-    // Logged in
-    if (user) {
-      return (
-        <Menu>
-          <MenuButton
-            as={Avatar}
-            name={user?.name ?? "User"}
-            src={user?.picture ?? null}
-            _hover={{ cursor: "pointer" }}
-            ml={{ base: 0, md: 6 }}
-            boxSize="10"
-          >
-            {user.email_verified ? (
-              <AvatarBadge boxSize="1em" bg="green.500" borderWidth="0.15em">
-                <FaCheck size="8" />
-              </AvatarBadge>
-            ) : (
-              <AvatarBadge boxSize="1em" bg="orange.500" borderWidth="0.15em">
-                <FaExclamation size="8" />
-              </AvatarBadge>
-            )}
-          </MenuButton>
-          <MenuList>
-            <MenuGroup title="Account">
-              <Link href="/user/profile">
-                <MenuItem>Profile</MenuItem>
-              </Link>
-              {/* <Link href="/user/my">
+  // Loading
+  if (isLoading) {
+    return (
+      <Button
+        colorScheme="blue"
+        variant="ghost"
+        size="md"
+        ml="10px"
+        mr="10px"
+        spinner={<BeatLoader size={8} color="white" />}
+        isLoading
+      />
+    );
+  }
+
+  // Logged in
+  if (user) {
+    return (
+      <Menu>
+        <MenuButton
+          as={Avatar}
+          name={user?.name ?? "User"}
+          src={user?.picture ?? null}
+          _hover={{ cursor: "pointer" }}
+          ml={{ base: 0, md: 6 }}
+          boxSize="10"
+        >
+          {user.email_verified ? (
+            <AvatarBadge boxSize="1em" bg="green.500" borderWidth="0.15em">
+              <FaCheck size="8" />
+            </AvatarBadge>
+          ) : (
+            <AvatarBadge boxSize="1em" bg="orange.500" borderWidth="0.15em">
+              <FaExclamation size="8" />
+            </AvatarBadge>
+          )}
+        </MenuButton>
+        <MenuList>
+          <MenuGroup title="Account">
+            <Link href="/user/profile">
+              <MenuItem>Profile</MenuItem>
+            </Link>
+            {/* <Link href="/user/my">
                 <MenuItem>最愛</MenuItem>
               </Link> */}
-            </MenuGroup>
-            <MenuDivider display={{ base: "block", md: "none" }} />
-            <MenuGroup
-              title="More"
-              display={{ base: "inline-block", md: "none" }}
+          </MenuGroup>
+          <MenuDivider display={{ base: "block", md: "none" }} />
+          <MenuGroup
+            title="More"
+            display={{ base: "inline-block", md: "none" }}
+          >
+            <Link href="/about">
+              <MenuItem display={{ base: "inline-block", md: "none" }}>
+                About
+              </MenuItem>
+            </Link>
+          </MenuGroup>
+          <MenuDivider />
+          <Flex justifyContent="end" alignItems="center">
+            <Flex
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="start"
+              m="2"
+              ml="4"
             >
-              <Link href="/about">
-                <MenuItem display={{ base: "inline-block", md: "none" }}>
-                  About
-                </MenuItem>
-              </Link>
-            </MenuGroup>
-            <MenuDivider />
-            <Flex justifyContent="end" alignItems="center">
-              <Flex
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="start"
-                m="2"
-                ml="4"
+              <Badge
+                colorScheme={user.email_verified ? "green" : "yellow"}
+                mb="1"
               >
-                <Badge
-                  colorScheme={user.email_verified ? "green" : "yellow"}
-                  mb="1"
-                >
-                  {user.email_verified ? "Verified" : "Unverified"}
-                </Badge>
-                <Text fontSize="sm" color={textColor} fontWeight="700">
-                  {user.name}
-                </Text>
-                <Text fontSize="xs" color={textColor} fontWeight="500">
-                  {user.email}
-                </Text>
-              </Flex>
-              <Button
-                colorScheme="red"
-                variant="outline"
-                size="md"
-                m="2"
-                mr="4"
-                onClick={() => {
+                {user.email_verified ? "Verified" : "Unverified"}
+              </Badge>
+              <Text fontSize="sm" color={textColor} fontWeight="700">
+                {user.name}
+              </Text>
+              <Text fontSize="xs" color={textColor} fontWeight="500">
+                {user.email}
+              </Text>
+            </Flex>
+            <Button
+              colorScheme="red"
+              variant="outline"
+              size="md"
+              m="2"
+              mr="4"
+              onClick={() => {
                 //   reportEvent("header", "click", "logout");
-                  router.push("/api/auth/logout");
-                }}
-              >
-                Logout
-              </Button>
-            </Flex>
-          </MenuList>
-        </Menu>
-      );
-    }
+                router.push("/api/auth/logout");
+              }}
+            >
+              Logout
+            </Button>
+          </Flex>
+        </MenuList>
+      </Menu>
+    );
+  }
 
-    // Not logged in
-    return (
-      <>
-        <Menu>
-          <MenuButton
-            as={Avatar}
-            _hover={{ cursor: "pointer" }}
-            display={{ base: "inline", md: "none" }}
-          />
-          <MenuList>
-            <MenuGroup title="更多">
-              <Link href="/about">
-                <MenuItem>About</MenuItem>
-              </Link>
-            </MenuGroup>
-            <MenuDivider />
-            <Flex justifyContent="end" alignItems="center">
-              <Button
-                colorScheme="yellow"
-                rightIcon={<ChevronRightIcon />}
-                variant="solid"
-                size="md"
-                m="2"
-                mr="4"
-                onClick={() => {
-                  reportEvent("header", "click", "login");
-                  router.push("/api/auth/login");
-                }}
-              >
-                登入 / 註冊
-              </Button>
-            </Flex>
-          </MenuList>
-        </Menu>
-        <Button
-          bg={loginBtnBg}
-          color={"gray.800"}
-          rightIcon={<ChevronRightIcon />}
-          size="md"
-          ml="10px"
-          mr="10px"
-          onClick={() => {
-            // reportEvent("header", "click_external", "status");
-            router.push("/api/auth/login");
-          }}
-          display={{ base: "none", md: "inline-block" }}
-        >
-          Login / Sign Up
-        </Button>
-      </>
-    );
-  }
-  
-  function NavBar() {
-    return (
-      <Flex
-        position="fixed"
-        w="100%"
-        h="64px"
-        // bg={useColorModeValue("headerBar.light", "headerBar.dark")}
-        bg="whiteAlpha.900"
-        flexDirection="row"
-        justifyContent="space-between"
-        alignItems="center"
-        zIndex="1000"
-        px={{ base: 6, md: 10 }}
+  // Not logged in
+  return (
+    <>
+      <Menu>
+        <MenuButton
+          as={Avatar}
+          _hover={{ cursor: "pointer" }}
+          display={{ base: "inline", md: "none" }}
+        />
+        <MenuList>
+          <MenuGroup title="More">
+            <Link href="/about">
+              <MenuItem>About</MenuItem>
+            </Link>
+          </MenuGroup>
+          <MenuDivider />
+          <Flex justifyContent="end" alignItems="center">
+            <Button
+              colorScheme="yellow"
+              rightIcon={<ChevronRightIcon />}
+              variant="solid"
+              size="md"
+              m="2"
+              mr="4"
+              onClick={() => {
+                reportEvent("header", "click", "login");
+                router.push("/api/auth/login");
+              }}
+            >
+              Login / Sign Up
+            </Button>
+          </Flex>
+        </MenuList>
+      </Menu>
+      <Button
+        bg={loginBtnBg}
+        color={"gray.800"}
+        rightIcon={<ChevronRightIcon />}
+        size="md"
+        ml="10px"
+        mr="10px"
+        onClick={() => {
+          // reportEvent("header", "click_external", "status");
+          router.push("/api/auth/login");
+        }}
+        display={{ base: "none", md: "inline-block" }}
       >
-        <Flex justifyContent="center" alignItems="center">
-          <Link href="/" passHref>
-            <Flex alignItems="center" flexDirection="row" cursor="pointer">
-              <Image
-                src={`/logo.png`}
-                alt="ncnLogo"
-                width="30"
-                height="30"
-                layout="fixed"
-              />
-              <Heading
-                ml="2"
-                fontSize={{ base: "lg", md: "xl" }}
-                fontWeight="700"
-                color={useColorModeValue("heading.light", "heading.dark")}
-                display={{ base: "none", md: "inline-block" }}
-              >
-                CCMeMan
-              </Heading>
-            </Flex>
-          </Link>
-          <Link href="/group">
-            <Button
-              variant="ghost"
-              size="md"
-              ml={{ base: 4, md: 6 }}
-              leftIcon={<FaRegCalendarAlt/>}
-              color={useColorModeValue("link.light", "link.dark")}
-            >
-              Groups / Meetings
-            </Button>
-          </Link>
-        </Flex>
-        <Flex justifyContent="center" alignItems="center">
-          <Link href="/about">
-            <Button
-              variant="ghost"
-              size="md"
-              ml="30px"
+        Login / Sign Up
+      </Button>
+    </>
+  );
+}
+
+function NavBar() {
+  return (
+    <Flex
+      position="fixed"
+      w="100%"
+      h="64px"
+      // bg={useColorModeValue("headerBar.light", "headerBar.dark")}
+      bg="whiteAlpha.900"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      zIndex="1000"
+      px={{ base: 6, md: 10 }}
+    >
+      <Flex justifyContent="center" alignItems="center">
+        <Link href="/" passHref>
+          <Flex alignItems="center" flexDirection="row" cursor="pointer">
+            <Image
+              src={`/logo.png`}
+              alt="ncnLogo"
+              width="30"
+              height="30"
+              layout="fixed"
+            />
+            <Heading
+              ml="2"
+              fontSize={{ base: "lg", md: "xl" }}
+              fontWeight="700"
+              color={useColorModeValue("heading.light", "heading.dark")}
               display={{ base: "none", md: "inline-block" }}
-              color={useColorModeValue("link.light", "link.dark")}
             >
-              <HStack>
-                <FaInfoCircle />
-                <Text>About</Text>
-              </HStack>
-            </Button>
-          </Link>
-          {/* <ThemeToggleButton /> */}
-          <SignInButton />
-        </Flex>
+              CCMeMan
+            </Heading>
+          </Flex>
+        </Link>
+        <Link href="/group">
+          <Button
+            variant="ghost"
+            size="md"
+            ml={{ base: 4, md: 6 }}
+            leftIcon={<FaRegCalendarAlt />}
+            color={useColorModeValue("link.light", "link.dark")}
+          >
+            Groups / Meetings
+          </Button>
+        </Link>
       </Flex>
-    );
-  }
-  
-  export default NavBar;
+      <Flex justifyContent="center" alignItems="center">
+        <Link href="/about">
+          <Button
+            variant="ghost"
+            size="md"
+            ml="30px"
+            display={{ base: "none", md: "inline-block" }}
+            color={useColorModeValue("link.light", "link.dark")}
+          >
+            <HStack>
+              <FaInfoCircle />
+              <Text>About</Text>
+            </HStack>
+          </Button>
+        </Link>
+        {/* <ThemeToggleButton /> */}
+        <SignInButton />
+      </Flex>
+    </Flex>
+  );
+}
+
+export default NavBar;
--- src/pages/group/index.js
+++ src/pages/group/index.js
@@ -27,8 +27,15 @@ import {
   Badge,
   useColorModeValue,
 } from "@chakra-ui/react";
+import {
+  CalendarIcon,
+  ChevronRightIcon,
+  TrashIcon,
+  PlusIcon,
+} from "@heroicons/react/20/solid";
 import ClipLoader from "react-spinners/ClipLoader";
 // import Select, { StylesConfig, GroupBase } from "react-select";
+import { DateTime } from "luxon";
 import axios from "axios";
 import { HashLoader } from "react-spinners";
 import { FaExclamationTriangle } from "react-icons/fa";
@@ -152,46 +159,173 @@ export default function UserGroupsPage() {
     );
   }
   return (
-    <Box my={{ base: 5, md: 10, lg: 10 }} mx={{ base: 5, md: 10, lg: 40 }}>
-      <Text fontSize="3xl" fontWeight="700" color="gray.800">
-        Groups
-      </Text>
-      <Divider mt="1" mb="4" />
-      <VStack
-        mx={{ base: 5, md: 10 }}
-        divider={<StackDivider borderColor="gray.200" />}
-        spacing={2}
-        align="stretch"
-      >
-        <Text mx={4} fontSize="2xl" fontWeight="700" color="gray.800">
-          Name
+    <>
+      {/* <Box my={{ base: 5, md: 10, lg: 10 }} mx={{ base: 5, md: 10, lg: 40 }}>
+        <Text fontSize="3xl" fontWeight="700" color="gray.800">
+          Groups
         </Text>
+        <Divider mt="1" mb="4" />
+        <VStack
+          mx={{ base: 5, md: 10 }}
+          divider={<StackDivider borderColor="gray.200" />}
+          spacing={2}
+          align="stretch"
+        >
+          <Text mx={4} fontSize="2xl" fontWeight="700" color="gray.800">
+            Name
+          </Text>
 
-        {existingGroups.map((item) => {
-          return <GroupItem key={item.id} group={item} />;
-        })}
+          {existingGroups.map((item) => {
+            return <GroupItem key={item.id} group={item} />;
+          })}
 
-        <Flex mx={4}>
-          <Input
-            mr={2}
-            type="text"
-            id="group-name"
-            placeholder="Name"
-            onChange={(e) => {
-              setNewGroupName(e.currentTarget.value);
-            }}
-          ></Input>
-          <Button
-            colorScheme="green"
-            onClick={async () => {
-              createGroup();
-            }}
-          >
-            Create Group{" "}
-          </Button>
-        </Flex>
-      </VStack>
-    </Box>
+          <Flex mx={4}>
+            <Input
+              mr={2}
+              type="text"
+              id="group-name"
+              placeholder="Name"
+              onChange={(e) => {
+                setNewGroupName(e.currentTarget.value);
+              }}
+            ></Input>
+            <Button
+              colorScheme="green"
+              onClick={async () => {
+                createGroup();
+              }}
+            >
+              Create Group{" "}
+            </Button>
+          </Flex>
+        </VStack>
+      </Box> */}
+
+      <div className="sm:px-4 lg:px-40 lg:pt-10 xl:px-60 2xl:px-80 2xl:pt-30">
+        <div className="border-b m-5 border-gray-200 pb-5">
+          <h3 className="text-2xl font-medium leading-6 text-gray-900">
+            Groups
+          </h3>
+        </div>
+        <div className="overflow-hidden bg-white shadow sm:rounded-md m-5">
+          <ul role="list" className="divide-y divide-gray-200">
+            {existingGroups?.map((item) => (
+              <li key={item.id}>
+                <div className="flex items-center px-4 py-4 sm:px-6 hover:bg-gray-50">
+                  <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                    <a
+                      href={"group/" + item.nanoId}
+                      className="hover:underline decoration-solid"
+                    >
+                      <div className="truncate">
+                        <div className="flex text-sm">
+                          <p className="truncate font-medium text-indigo-600">
+                            {item.name}
+                          </p>
+                          <p className="ml-1 flex-shrink-0 font-normal text-gray-500">
+                            {/* in [parentGroup] */}
+                          </p>
+                        </div>
+                        <div className="mt-2 flex">
+                          <div className="flex items-center text-sm text-gray-500">
+                            <CalendarIcon
+                              className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                              aria-hidden="true"
+                            />
+                            <p>
+                              Last updated on{" "}
+                              <time dateTime={item.updatedAt}>
+                                {DateTime.fromISO(item.updatedAt).toFormat(
+                                  "y LLL. d HH:mm:ss ZZZZ"
+                                )}
+                              </time>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="mt-4 flex-shrink-0 sm:mt-0 sm:ml-5">
+                        {/* <div className="flex -space-x-1 overflow-hidden">
+                        {position.applicants.map((applicant) => (
+                          <img
+                            key={applicant.email}
+                            className="inline-block h-6 w-6 rounded-full ring-2 ring-white"
+                            src={applicant.imageUrl}
+                            alt={applicant.name}
+                          />
+                        ))}
+                      </div> */}
+                      </div>
+                    </a>
+                  </div>
+                  <button
+                    type="button"
+                    className="inline-flex items-center rounded-md 
+                    disabled:opacity-50
+                      border border-transparent
+                       bg-red-600 px-4 py-2 text-sm font-medium
+                       text-white shadow-sm
+                       enabled:hover:bg-red-700 enabled:active:bg-red-800"
+                    // disabled
+                  >
+                    <TrashIcon
+                      className="-ml-1 mr-2 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                    Delete
+                  </button>
+                  <div className="ml-5 flex-shrink-0">
+                    <ChevronRightIcon
+                      className="h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                </div>
+              </li>
+            ))}
+            <li>
+              <div className="flex items-center px-4 py-4 sm:px-6 bg-gray-50">
+                <div className="min-w-0 flex-1 sm:flex sm:items-center sm:justify-between">
+                  <input
+                    placeholder="New Group Name"
+                    className="block w-full mr-10 px-4 h-10 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    onChange={(e) => {
+                      setNewGroupName(e.currentTarget.value);
+                    }}
+                  />
+                </div>
+                <button
+                  type="button"
+                  className="inline-flex items-center rounded-md 
+                    disabled:opacity-50
+                      border border-transparent
+                       bg-indigo-500 px-4 py-2 text-sm font-medium
+                       text-white shadow-sm
+                       enabled:hover:bg-indigo-700 enabled:active:bg-indigo-800"
+                  onClick={async () => {
+                    createGroup();
+                  }}
+                >
+                  <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                  Create
+                </button>
+                <div className="ml-5 flex-shrink-0">
+                  <div className="h-5 w-5" aria-hidden="true" />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="h-5"></div>
+        <div className="border-b m-5 border-gray-200 pb-5">
+          <h3 className="text-2xl font-medium leading-6 text-gray-900">
+            Debug Info.
+          </h3>
+        </div>
+        <div className="text-gray-300 px-10">
+          {JSON.stringify(existingGroups)}
+        </div> */}
+      </div>
+    </>
   );
 }
 
