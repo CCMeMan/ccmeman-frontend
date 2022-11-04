a8f9150d82d2a6f1107457a9a4be444ac61650dd
Cory Chu <cory@gwlab.page>
2022-11-04 16:50:36 -0500

    customized theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+          // Color Picker: https://imagecolorpicker.com/color-code/36d7b7
+          // https://tailwindcss.com/docs/customizing-colors
+          // "indigo": {
+          //   500: "#36d7b7",
+          //   600: "#2bac92",
+          //   700: "#20816e",
+          // },
+        },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
